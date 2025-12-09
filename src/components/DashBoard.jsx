9b4f65cab23fa25562f6d909aa92036db26af266
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-12-09 17:25:25 +0100

    ........

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -14,8 +14,8 @@ const DashBoard = () => {
 
       <button
         onClick={() => dispatch(loginOut())}
-        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
-      >
+        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">
+      
         Logout
       </button>
     </div>
