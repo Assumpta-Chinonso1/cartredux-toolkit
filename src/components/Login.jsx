a3c37a67414416c144c0df06c9fd18c0f0676871
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-12-11 16:28:52 +0100

    ........

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -80,7 +80,7 @@ const Login = () => {
         className="border p-2 w-full rounded mb-3 dark:bg-gray-700 dark:text-white"
         value={password}
         onChange={(e) => setPassword(e.target.value)} />
-      />
+     
       <button
         onClick={handleLogin}
         className="w-full bg-blue-600 text-white p-2 rounded disabled:bg-blue-300"
