f4523e292df52f3422e46e0fb2782d6e4d4e4e5f
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-12-11 16:30:14 +0100

    .......

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -8,8 +8,8 @@ export default function DashBoard() {
     <div className="max-w-sm ml-1 mt-2 p-6">
       <button
         onClick={() => dispatch(logout())}
-        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
-      >
+        className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition">
+     
         Logout
       </button>
     </div>
